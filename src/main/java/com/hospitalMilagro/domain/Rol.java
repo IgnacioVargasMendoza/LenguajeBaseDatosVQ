ab6d928ff8dff4ab5eb768dde5001a9ba9c4c567
Ignacio Vargas <ignacii31@gmail.com>
2025-03-18 13:04:11 -0600

    Reparacion de Clase Domain
    Se agregaron los atributos de la clase Rol y Usuario Rol

--- src/main/java/com/hospitalMilagro/domain/Rol.java
+++ src/main/java/com/hospitalMilagro/domain/Rol.java
@@ -1,10 +1,39 @@
 
 package com.hospitalMilagro.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import java.util.List;
+import lombok.Data;
+
 /**
  *
  * @author Ignac
  */
-public class Rol {
+@Data
+@Entity
+@Table(name ="FIDE_ROL_TB")
+public class Rol implements Serializable{
     
+        private static final long serialVersionUID = 1L;
+        
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        @Column(name = "ID_ROL")
+        private Long idRol;
+        @Column(name = "NOMBRE")
+        private String nombreRol;
+        @Column(name = "ACTIVO")
+        private short activo;
+        
+        @OneToMany
+        @JoinColumn(name = "ID_ROL")
+        private List<UsuarioRol> usuarioRol;
 }
--- src/main/java/com/hospitalMilagro/domain/UsuarioRol.java
+++ src/main/java/com/hospitalMilagro/domain/UsuarioRol.java
@@ -1,9 +1,38 @@
 package com.hospitalMilagro.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.Data;
+
 /**
  *
  * @author Ignac
  */
-public class UsuarioRol {
+@Data
+@Entity
+@Table(name = "FIDE_USUARIO_ROL_TB")
+public class UsuarioRol implements Serializable {
+    
+    private static final Long serialVersionUID =1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_USUARIO_ROL")
+    private Long idUsuarioRol;
+
+    @ManyToOne
+    @JoinColumn(name = "id_usuario", insertable=false, updatable=false)
+    private Usuario usuario;
+
+    @ManyToOne
+    @JoinColumn(name = "id_rol", insertable=false, updatable=false)
+    private Rol rol;
     
 }
