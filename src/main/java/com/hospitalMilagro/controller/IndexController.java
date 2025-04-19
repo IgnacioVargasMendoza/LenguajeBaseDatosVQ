dba229a75a0412cc97afd9db228d668d8341c5f8
Ignacio Vargas <ignacii31@gmail.com>
2025-04-19 15:55:21 -0600

    Moviento de archivo thml Login

--- src/main/java/com/hospitalMilagro/controller/IndexController.java
+++ src/main/java/com/hospitalMilagro/controller/IndexController.java
@@ -1,7 +1,6 @@
 package com.hospitalMilagro.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -12,9 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class IndexController {
     
     @RequestMapping("/")
-    public String page(Model model) {
-        model.addAttribute("attribute", "value");
-        return "/login";
+    public String page() {
+        return "login/login";
     }
     
 }
