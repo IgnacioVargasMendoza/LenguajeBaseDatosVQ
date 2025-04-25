c65cec7f3d2abf53ebe0b8e622d555c522dc5215
Caleb-Sacasa <caleborus24@gmail.com>
2025-04-25 11:12:12 -0600

    update inserciones direciones

--- DB_Hospital-Milagro/InsercionesDirecciones.sql
+++ DB_Hospital-Milagro/InsercionesDirecciones.sql
@@ -1,9 +1,9 @@
--- Script para insertar países, cantones, distritos y direcciones (todas en Costa Rica)
+-- Script para insertar paï¿½ses, cantones, distritos y direcciones (todas en Costa Rica)
 
--- 1. Insertar país Costa Rica
+-- 1. Insertar paï¿½s Costa Rica
 EXEC FIDE_REGISTRAR_PAIS_SP('Costa Rica');
 
--- Verificar el ID de país insertado
+-- Verificar el ID de paï¿½s insertado
 -- SELECT * FROM FIDE_PAISES_TB;
 -- Asumo que el ID es 1
 
@@ -12,38 +12,38 @@ SELECT * FROM FIDE_CANTONES_TB;
 SELECT * FROM FIDE_DISTRITOS_TB;
 
 -- 2. Insertar cantones de Costa Rica (ID_PAIS = 1)
-EXEC FIDE_REGISTRAR_CANTON_SP('San José', 1);
+EXEC FIDE_REGISTRAR_CANTON_SP('San Josï¿½', 1);
 EXEC FIDE_REGISTRAR_CANTON_SP('Alajuela', 1);
 EXEC FIDE_REGISTRAR_CANTON_SP('Cartago', 1);
 EXEC FIDE_REGISTRAR_CANTON_SP('Heredia', 1);
 EXEC FIDE_REGISTRAR_CANTON_SP('Guanacaste', 1);
 EXEC FIDE_REGISTRAR_CANTON_SP('Puntarenas', 1);
-EXEC FIDE_REGISTRAR_CANTON_SP('Limón', 1);
+EXEC FIDE_REGISTRAR_CANTON_SP('Limï¿½n', 1);
 
 -- Verificar los IDs de cantones insertados
 -- SELECT * FROM FIDE_CANTONES_TB;
 -- Asumo que los IDs son del 1 al 7 respectivamente
 
 -- 3. Insertar distritos
--- Distritos de San José (ID_CANTON = 1)
-EXEC FIDE_REGISTRAR_DISTRITO_SP('Escazú', 100);
+-- Distritos de San Josï¿½ (ID_CANTON = 1)
+EXEC FIDE_REGISTRAR_DISTRITO_SP('Escazï¿½', 100);
 EXEC FIDE_REGISTRAR_DISTRITO_SP('Santa Ana', 100);
 EXEC FIDE_REGISTRAR_DISTRITO_SP('Desamparados', 100);
 
 -- Distritos de Alajuela (ID_CANTON = 2)
 EXEC FIDE_REGISTRAR_DISTRITO_SP('Grecia', 101);
-EXEC FIDE_REGISTRAR_DISTRITO_SP('San Ramón', 101);
+EXEC FIDE_REGISTRAR_DISTRITO_SP('San Ramï¿½n', 101);
 EXEC FIDE_REGISTRAR_DISTRITO_SP('Atenas', 101);
 
 -- Distritos de Cartago (ID_CANTON = 3)
-EXEC FIDE_REGISTRAR_DISTRITO_SP('Tres Ríos', 102);
+EXEC FIDE_REGISTRAR_DISTRITO_SP('Tres Rï¿½os', 102);
 EXEC FIDE_REGISTRAR_DISTRITO_SP('Turrialba', 102);
 EXEC FIDE_REGISTRAR_DISTRITO_SP('El Guarco', 102);
 
 -- Distritos de Heredia (ID_CANTON = 4)
 EXEC FIDE_REGISTRAR_DISTRITO_SP('Santo Domingo', 103);
 EXEC FIDE_REGISTRAR_DISTRITO_SP('San Rafael', 103);
-EXEC FIDE_REGISTRAR_DISTRITO_SP('Belén', 103);
+EXEC FIDE_REGISTRAR_DISTRITO_SP('Belï¿½n', 103);
 
 -- Distritos de Guanacaste (ID_CANTON = 5)
 EXEC FIDE_REGISTRAR_DISTRITO_SP('Liberia', 104);
@@ -53,44 +53,44 @@ EXEC FIDE_REGISTRAR_DISTRITO_SP('Nicoya', 104);
 EXEC FIDE_REGISTRAR_DISTRITO_SP('Esparza', 105);
 EXEC FIDE_REGISTRAR_DISTRITO_SP('Quepos', 105);
 
--- Distritos de Limón (ID_CANTON = 7)
+-- Distritos de Limï¿½n (ID_CANTON = 7)
 EXEC FIDE_REGISTRAR_DISTRITO_SP('Siquirres', 106);
-EXEC FIDE_REGISTRAR_DISTRITO_SP('Guápiles', 106);
+EXEC FIDE_REGISTRAR_DISTRITO_SP('Guï¿½piles', 106);
 
 -- Verificar los IDs de distritos insertados
 -- SELECT * FROM FIDE_DISTRITOS_TB;
 -- Asumo que los IDs son del 1 al 17 respectivamente
 
 -- 4. Insertar 10 direcciones (todas en Costa Rica)
--- Dirección 1: Costa Rica, San José, Escazú
-EXEC FIDE_REGISTRAR_DIRECCION_SP(1, 100, 10018);
+-- Direcciï¿½n 1: Costa Rica, San Josï¿½, Escazï¿½
+EXEC FIDE_REGISTRAR_DIRECCION_SP(1, 100, 10000);
 
--- Dirección 2: Costa Rica, San José, Santa Ana
-EXEC FIDE_REGISTRAR_DIRECCION_SP(1, 100, 10019);
+-- Direcciï¿½n 2: Costa Rica, San Josï¿½, Santa Ana
+EXEC FIDE_REGISTRAR_DIRECCION_SP(1, 100, 10001);
 
--- Dirección 3: Costa Rica, San José, Desamparados
-EXEC FIDE_REGISTRAR_DIRECCION_SP(1, 100, 10020);
+-- Direcciï¿½n 3: Costa Rica, San Josï¿½, Desamparados
+EXEC FIDE_REGISTRAR_DIRECCION_SP(1, 100, 10002);
 
--- Dirección 4: Costa Rica, Alajuela, Grecia
-EXEC FIDE_REGISTRAR_DIRECCION_SP(1, 101, 10021);
+-- Direcciï¿½n 4: Costa Rica, Alajuela, Grecia
+EXEC FIDE_REGISTRAR_DIRECCION_SP(1, 101, 10003);
 
--- Dirección 5: Costa Rica, Alajuela, San Ramón
-EXEC FIDE_REGISTRAR_DIRECCION_SP(1, 101, 10022);
+-- Direcciï¿½n 5: Costa Rica, Alajuela, San Ramï¿½n
+EXEC FIDE_REGISTRAR_DIRECCION_SP(1, 101, 10004);
 
--- Dirección 6: Costa Rica, Cartago, Tres Ríos
-EXEC FIDE_REGISTRAR_DIRECCION_SP(1, 102, 10024);
+-- Direcciï¿½n 6: Costa Rica, Cartago, Tres Rï¿½os
+EXEC FIDE_REGISTRAR_DIRECCION_SP(1, 102, 10006);
 
--- Dirección 7: Costa Rica, Heredia, Santo Domingo
-EXEC FIDE_REGISTRAR_DIRECCION_SP(1, 103, 10027);
+-- Direcciï¿½n 7: Costa Rica, Heredia, Santo Domingo
+EXEC FIDE_REGISTRAR_DIRECCION_SP(1, 103, 10009);
 
--- Dirección 8: Costa Rica, Guanacaste, Liberia
-EXEC FIDE_REGISTRAR_DIRECCION_SP(1, 104, 10030);
+-- Direcciï¿½n 8: Costa Rica, Guanacaste, Liberia
+EXEC FIDE_REGISTRAR_DIRECCION_SP(1, 104, 10012);
 
--- Dirección 9: Costa Rica, Puntarenas, Esparza
-EXEC FIDE_REGISTRAR_DIRECCION_SP(1, 105, 10032);
+-- Direcciï¿½n 9: Costa Rica, Puntarenas, Esparza
+EXEC FIDE_REGISTRAR_DIRECCION_SP(1, 105, 10014);
 
--- Dirección 10: Costa Rica, Limón, Siquirres
-EXEC FIDE_REGISTRAR_DIRECCION_SP(1, 106, 10034);
+-- Direcciï¿½n 10: Costa Rica, Limï¿½n, Siquirres
+EXEC FIDE_REGISTRAR_DIRECCION_SP(1, 106, 10016);
 
 -- Verificar las direcciones insertadas
  SELECT * FROM FIDE_DIRECCION_TB;
